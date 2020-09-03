9b475238660a554c8b34b13aff57a0614ab02540
Jean-Marie PARENT <jmparent.github@gmail.com>
2020-09-03 13:17:27 +0200

    Init App

---
+++ src/ZlanMobileApp/ZlanMobileApp/ZlanMobileApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZlanMobileApp.Services;
+using ZlanMobileApp.Views;
+
+namespace ZlanMobileApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/ZlanMobileApp/ZlanMobileApp/ZlanMobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ZlanMobileApp.ViewModels;
+
+namespace ZlanMobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
